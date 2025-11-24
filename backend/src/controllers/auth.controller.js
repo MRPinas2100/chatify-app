f68ca3dd76df96ab785c6d8aeff9fd049b0b7874
Andrés Felipe Salamanca Silva <56135223+MRPinas2100@users.noreply.github.com>
2025-11-23 21:16:52 -0500

    signup controller added, logic for jwt and sending cookies with it

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+import { generateToken, send_JTW_To_Cookies } from "../lib/utils.js"
+import { User } from "../models/User.js"
+import bcrypt from "bcryptjs"
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" })
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" })
+    }
+
+    //Check if email is valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const isEmailValid = emailRegex.test(email)
+    if (!isEmailValid) {
+      return res.status(400).json({ message: "Invalid email format" })
+    }
+
+    const user = await User.findOne({ email })
+    if (user) return res.status(400).json({ message: "Email already exists" })
+
+    //make secure passwords
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    })
+
+    if (newUser) {
+      const token = generateToken(newUser._id)
+      send_JTW_To_Cookies(token, res)
+      await newUser.save()
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      })
+    } else {
+      res.status(400).json({ message: "Invalid user" })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Internal error" })
+  }
+}
