3b20475e5031d820611255748a536308eb97f6b7
Andrés Felipe Salamanca Silva <56135223+MRPinas2100@users.noreply.github.com>
2025-11-23 19:00:34 -0500

    Adding package json to main leve to build one integret for savala

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import messagesRoutes from "./routes/messages.route.js"
 
 dotenv.config()
 const app = express()
+const __dirname = path.resolve()
 const PORT = process.env.PORT || 3000
 
 app.use("/api/auth", authRoutes)
