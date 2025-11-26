970221f8f6cfc20b6519195dd947100c0ae6242a
Andrés Felipe Salamanca Silva <56135223+MRPinas2100@users.noreply.github.com>
2025-11-26 18:33:16 -0500

    Messages optimized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -64,6 +64,24 @@ export const sendMessage = async (req, res) => {
   const { id: receiverId } = req.params
   const senderId = req.user._id
   let imageUrl = null
+
+  if (!text && !!image) {
+    return res.status(400).json({ message: "Text or Image is required." })
+  }
+
+  if (senderId.equals(receiverId)) {
+    return res.status(400).json({ message: "Cannot send message to yourself." })
+  }
+
+  const receiverExists = await User.exists({ _id: receiverId })
+  if (!receiverExists) {
+    return res.status(404).json({ message: "Receiver not found." })
+  }
+
+  if (!text && !!image) {
+    return res.status(400).json({ message: "Text or Image is required." })
+  }
+
   try {
     if (image) {
       //upload base64 image to cloudinary
