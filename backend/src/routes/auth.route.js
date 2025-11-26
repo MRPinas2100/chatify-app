34c53a8debd1e7b443e3982a6be133270bca5e5c
Andrés Felipe Salamanca Silva <56135223+MRPinas2100@users.noreply.github.com>
2025-11-25 21:11:22 -0500

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -10,10 +10,11 @@ import { protectRoute } from "../middleware/auth.middleware.js"
 import { arcjetProtection } from "../middleware/arcjet.middleware.js"
 
 const router = express.Router()
+router.use(arcjetProtection)
 
-router.post("/singup", arcjetProtection, signup)
-router.post("/login", arcjetProtection, login)
-router.post("/logout", arcjetProtection, logout)
+router.post("/singup", signup)
+router.post("/login", login)
+router.post("/logout", logout)
 router.put("/update-profile", protectRoute, updateProfile)
 router.get("/check", protectRoute, check)
 
